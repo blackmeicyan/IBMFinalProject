49adbf1b1be9aff6990e66c39510c54eb644a997
blackmeicyan <blackmeicyan@gmail.com>
2025-05-22 11:53:31 +1000

    Update script1.js

--- script1.js
+++ script1.js
@@ -12,12 +12,11 @@ function compute() {
         document.getElementById("principal").focus();
     }
     else {
-        result.innerHTML = "If you deposit $" + "<mark>" + principal + "</mark>" + ",\<br\> at an interest rate of " + "<mark>" + rate + "%" + "</mark>" + "\<br\> You will receive an amount of $" + "<mark>" + amount + "</mark>" + ",\<br\> in the year " + "<mark>" + year + "</mark>" + "\<br\>";
+        result.innerHTML = "If you deposit $" + "<mark>" + principal + "</mark>" + 
+            ",\<br\> at an interest rate of " + "<mark>" + rate + "%" + "</mark>" + 
+            "\<br\> You will receive an amount of $" + "<mark>" + amount + "</mark>" + 
+            ",\<br\> in the year " + "<mark>" + year + "</mark>" + "\<br\>";
     } 
 }
 
-function updateRate()
-{
-    var rateval = document.getElementById("rate").value;
-    document.getElementById("rate_val").innerText = rateval;
-}
+
